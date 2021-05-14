57caeeb49420e51bb90b265ba499a495c48dc7ab
nothingistrue <nothingistrue@fakeemail.com>
2021-05-14 11:21:13 +0800

    Add NormalBandit

--- bandits/bandits.py
+++ bandits/bandits.py
@@ -1,8 +1,9 @@
 import random
+import numpy as np
 
 
 class BernoulliBandit:
-    """ A toy epsilon greedy bandit that rewards 1 if win and 0 if lose"""
+    """ A toy bernoulli bandit that rewards 1 if win and 0 if lose"""
 
     def __init__(self, win_rate=0.5):
         self.win_rate = win_rate
@@ -14,3 +15,14 @@ class BernoulliBandit:
             return 1
         else:
             return 0
+
+class NormalBandit:
+    """ A toy gaussian bandit that rewards 1 if win and 0 if lose based on a normal distribution"""
+
+    def __init__(self, mu, sigma=1):
+        self.mu = mu
+        self.sigma = sigma
+
+    def pull(self):
+        """ Return a reward based on the probability distribution """
+        return np.random.normal(self.mu, self.sigma)
